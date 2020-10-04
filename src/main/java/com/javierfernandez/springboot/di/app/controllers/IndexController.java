7d9e91b9d77510e8380251b2393aea1c0994544a
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-04 14:24:50 +0200

    desacoplacion con autoweird

--- src/main/java/com/javierfernandez/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/javierfernandez/springboot/di/app/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.javierfernandez.springboot.di.app.controllers;
 
 
 import com.javierfernandez.springboot.di.app.models.service.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private MiServicio servicio = new MiServicio();
+   // con autoweird no instancio
+   // private MiServicio servicio = new MiServicio();
 
+    @Autowired
+    private MiServicio servicio;
     @GetMapping({"/index","/",""})
     public String index(Model model){
 
--- src/main/java/com/javierfernandez/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/javierfernandez/springboot/di/app/models/service/MiServicio.java
@@ -1,5 +1,9 @@
 package com.javierfernandez.springboot.di.app.models.service;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class MiServicio {
 
     public String operacion(){
