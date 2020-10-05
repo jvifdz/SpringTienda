7f569e22d58e9c39f7712ee7706bad8a33b30327
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-05 12:11:13 +0200

    clase configuration añadiendo mas beans y eligiendo en el controlodaro con el qualifier

--- src/main/java/com/javierfernandez/springboot/di/app/AppConfig.java
+++ src/main/java/com/javierfernandez/springboot/di/app/AppConfig.java
@@ -3,15 +3,24 @@ package com.javierfernandez.springboot.di.app;
 
 import com.javierfernandez.springboot.di.app.models.service.IServicio;
 import com.javierfernandez.springboot.di.app.models.service.MiServicio;
+import com.javierfernandez.springboot.di.app.models.service.MiServicioComplejo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 
 public class AppConfig {
     @Bean("miServicioSimple")
+    @Primary
     public IServicio registrarMiServicio(){
 
         return new MiServicio();
+    }
+    @Bean("miServicioComplejo")
+
+    public IServicio registrarMiServicioComplejo(){
+
+        return new MiServicioComplejo();
     }
 }
