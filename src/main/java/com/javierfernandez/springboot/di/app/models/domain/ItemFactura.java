6a38d22eb1980305fad5f96b3af5efdfa1a09d1a
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-05 13:42:32 +0200

    Creada vista controlador de facturas

--- src/main/java/com/javierfernandez/springboot/di/app/models/domain/ItemFactura.java
+++ src/main/java/com/javierfernandez/springboot/di/app/models/domain/ItemFactura.java
@@ -26,4 +26,9 @@ public class ItemFactura {
     public void setCantidad(Integer cantidad) {
         this.cantidad = cantidad;
     }
+
+    public Integer calcularImporte(){
+        return cantidad*producto.getPrecio();
+
+    }
 }
